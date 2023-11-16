9486f56b01de33761ccd587f65479dda366fbad0
crimsonantmk <crimsonant69@gmail.com>
2023-11-16 11:37:25 -0800

    1 list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,18 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in h
- */
+ * list_len - Calculate the number of elements.
+ * @h: pointer to nest node in the list.
+ * Return: number.
+ **/
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	const list_t *tmp_store;
+	size_t i = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	for (tmp_store = h; tmp_store != NULL; tmp_store = tmp_store->next)
+	{
+		i++;
+	}
+	return (i);
 }
