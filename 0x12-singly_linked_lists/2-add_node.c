af33f4991e5e658247e29f54c607ed06dea72824
crimsonantmk <144614414+crimsonantmk@users.noreply.github.com>
2023-11-17 01:37:35 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,8 +14,8 @@ list_t *add_node(list_t **head, const char *str)
  list_t *new;
  unsigned int len = 0;
 
- while (str[len])
- len++;
+for (; str[len]; len++)
+		;
 
  new = malloc(sizeof(list_t));
  if (!new)
