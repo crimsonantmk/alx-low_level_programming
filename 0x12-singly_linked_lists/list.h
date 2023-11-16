1968bb48b2cb681dadb4ec2b01312ea947ceb7ee
crimsonantmk <crimsonant69@gmail.com>
2023-11-16 11:20:31 -0800

    lst

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -3,5 +3,24 @@
 
 #include <stdio.h>
 #include <stdlib>
+#include <string.h>
+
+
+size_t print_list(const list_t *h);
+
+/**
+ * struct list_s - singly linked list
+ * @str:string (malloc string)
+ * len: lenth of string
+ * @next: pointer to the next node
+ */
+
+struct list_s
+{
+	char *str;
+	size_t len;
+	struct list_s *next;
+}list_t;
+
 
 #endif
